d1cd7ee52a99da4079f7e2bdd78e115eaf8269ee
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-07-29 14:56:50 +0900

    programmers_예산

--- JS/solution.js
+++ JS/solution.js
@@ -1,15 +1,19 @@
-function solution(numbers) {
-  var answer = [];
-  var temp = [];
+function solution(d, budget) {  // d는 부서별 금액 배열 / budget는 총 예산
+  var answer = 0;
+  var temp = 0, result = 0, index_num = 0;
   
-  for(let i=0; i<numbers.length-1; i++) {
-      for(let j=i+1; j<numbers.length; j++) {
-          temp.push(numbers[i]+numbers[j]);
+  while(1) {
+      temp = Math.min.apply(null, d)          // 젤 작은 값 구함
+      if(result+temp <= budget) {
+          result += parseInt(temp);
+          
+          index_num = d.indexOf(temp);  // 가장 작은 값의 인덱스 찾음
+          d.splice(index_num, 1);       // 가장 작은 수 제거
+          answer++;            
       }
-      //console.log("");
+      else {
+          break;
+      }
   }
-  answer = [...new Set(temp)];
-  answer.sort((a,b) => a - b) //sort() ASCII값으로 정렬임됨 -> 그래서 숫자 정렬이랑 다르게 나옴
-  
   return answer;
 }
