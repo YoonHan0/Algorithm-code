e478cf767c34a3ab40977b5c896f658a3ee0a01d
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-08-06 23:20:41 +0900

    programmers_K번째수(2)

--- JS/K번째수.js
+++ JS/K번째수.js
@@ -4,12 +4,9 @@ function solution(array, commands) {
     for(let i=0; i<commands.length; i++) {
         let temp = array.slice(commands[i][0]-1, commands[i][1]);
         
-        temp.sort();
-        //console.log(temp);
-        console.log(temp[commands[i][2]]-1);
-        answer.push(temp[commands[i][2]]-1);
-
+        temp.sort((a,b) => a-b);
+        
+        answer.push(temp[commands[i][2]-1]);
     }
     return answer;
 }
-
